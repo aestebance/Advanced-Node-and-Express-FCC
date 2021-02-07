c5a18572ccc0f92028cbbb9b0607c88d25a69e7f
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-02-07 01:34:45 +0100

    challenge how to put a profile together

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ myDB(async client => {
 
   app.route('/profile')
   .get(ensureAuthenticated, (req, res) => {
-    res.render('pug/profile');
+    res.render('pug/profile', {"username": req.user.username});
   });
 
   passport.serializeUser((user, done) => {
