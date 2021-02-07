ef8aced55b84b510c36ee594604b72037c78ffb6
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-02-07 11:49:18 +0100

    authentication with socket.io

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const app = express();
 app.set('view engine', 'pug');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({url: URI});
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -21,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
+  store: store,
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false }
@@ -33,11 +39,21 @@ myDB(async client => {
   const myDataBase = await client.db('freeCodeCamp').collection('users');
   routes(app, myDataBase);
   auth(app, myDataBase);
+  io.use(
+      passportSocketIo.authorize({
+        cookieParser: cookieParser,
+        key: 'express.sid',
+        secret: process.env.SESSION_SECRET,
+        store: store,
+        success: onAuthorizeSuccess,
+        fail: onAuthorizeFail
+      })
+  );
   let currentUsers = 0;
   io.on('connection', socket => {
     ++currentUsers;
     io.emit('user count', currentUsers);
-    console.log('A user has connected');
+    console.log('user ' + socket.request.user.name + ' connected');
 
     socket.on('disconnect', () => {
       console.log("A user has disconnected");
@@ -51,6 +67,18 @@ myDB(async client => {
     res.render('pug/index', { title: e, message: 'Unable to login' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
